8569e8befd663363315e8548299fd5f76643820b
Ryan Ghaida <ryaboug@gmail.com>
2020-02-07 10:45:59 -0500

    added auth controllers

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,16 @@
+module CurrentUserConcern 
+  extend ActiveSupport::Concern
+
+  included do 
+    before_action :set_current_user
+  end 
+
+  def set_current_user
+    session["init"] = true 
+
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end 
+  end 
+  
+end 
