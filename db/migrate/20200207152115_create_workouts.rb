a969eb73cc4c2793d4d81b7ff7732373657dcfb0
Ryan Ghaida <ryaboug@gmail.com>
2020-02-07 10:37:48 -0500

    created database

--- db/migrate/20200207152115_create_workouts.rb
+++ db/migrate/20200207152115_create_workouts.rb
@@ -1,7 +1,8 @@
 class CreateWorkouts < ActiveRecord::Migration[6.0]
   def change
     create_table :workouts do |t|
-      
+      t.integer :user_id
+      t.integer :reps
       t.timestamps
     end
   end
