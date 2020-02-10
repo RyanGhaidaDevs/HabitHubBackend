a4d1adf8b37963633504b6d4843dd5a97827f265
Ryan Ghaida <ryaboug@gmail.com>
2020-02-10 13:51:55 -0500

    can save workout reps

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,16 @@
 class WorkoutsController < ApplicationController
+  include CurrentUserConcern
+
+  def create
+    params = workout_params
+    @workout = Workout.create(params)
+    render json: {workout: @workout}
+  end 
+
+  private 
+ 
+  def workout_params 
+    params.require(:user).permit(:user_id, :reps)
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
   resources :registrations, only: [:create, :index]
+
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+
+  post :reps, to: "workouts#create"
+  
 end
